c868f90b9ae2bafe6c755dadd7bf86b9fb7b3ee5
Mark van der Pas <mark.van.der.pas@semaku.com>
2023-11-20 16:38:20 +0100

    Add scripts to execute examples and update README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # production-trace-patterns
-Example data set and patterns for the paper about Production Trace Patterns
+Example data set and patterns accompanying the paper about Production Trace Patterns.
+
+## Content
+
+* `event-data/` contains the example data set (Turtle), split into three files following three ISA-95 levels.
+* `sparql/` contains the (template) SPARQL queries that implement the patterns and can be used to derive the 'hidden' information.
+* `apply_pattern_use_cases.py` script to execute the pattern queries for a set of use cases on the data in `event-data/`. The results are stored in `results/`.
+* `apply_pattern_combined.py` script to execute certain pattern queries in sequence on the data in `event-data/` to demonstrate how patterns can be combined to derive additional information. The results are stored in `results_combined/`.
+
+## Dependencies
+Execution of the scripts to apply the patterns requires [rdflib](https://github.com/RDFLib) and its dependencies (BSD 3-clause License) to be installed.
---
+++ apply_pattern_combined.py
@@ -0,0 +1,35 @@
+from rdflib import Graph, Namespace
+from time import time
+
+PTP = Namespace("http://example.org/def/production-trace-patterns/")
+
+if __name__=="__main__":
+
+    # Load data into one graph
+    g = Graph()
+    g.parse("event-data/isa-95_1.ttl")
+    g.parse("event-data/isa-95_2.ttl")
+    g.parse("event-data/isa-95_3.ttl")
+
+    # Define the patterns that have to be executed in order and corresponding parameters
+    # In this example first additional relations are derived using the ptp:isPartOf relation, followed by an aggregation
+    patterns_combined = [
+        ("event-entity_partOf.rq", {}),
+        ("interval_aggregate-attribute.rq", {"IntervalStartType": PTP.TrackIn, "IntervalEndType": PTP.TrackOut, "attribute": PTP.measuredValue}),
+    ]
+
+    # Execute the pattern queries
+    for query_file,bindings in patterns_combined:
+        with open(f"sparql/{query_file}") as f:
+            query = f.read()
+
+        start = time()
+        r = g.query(query, initBindings=bindings)
+
+        # If the result is a graph, add the data to the in-memory graph, otherwise store as CSV
+        if r.type == "CONSTRUCT":
+            g.parse(data=r.serialize(format="turtle"))
+        else:
+            r.serialize(f"results_combined/{query_file.replace('.rq', '')}_{hash(str(bindings))}.csv", format="csv")
+
+        print(f"{query_file:<32}: {time() - start} s")
